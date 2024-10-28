514ad0b2fded67a3f88bc4df7ab9ea6f1fb7edd1
coderoots <coderoots@coderootss-Mac-mini.local>
2024-10-28 22:56:22 +0000

    background error

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
 <style>
-        .background-wrapper {
-            background-image: url('{{ asset('images/books.jpeg') }}');
-            background-size: cover;
-            background-position: center;
-            background-attachment: fixed;
-            min-height: 100vh;
-            
-        }
-    </style>
+    .background-wrapper {
+        background-image: url("{{ asset('images/books.jpeg') }}");
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        min-height: 100vh;
+    }
+</style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
